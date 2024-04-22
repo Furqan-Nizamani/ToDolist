9182e260482ca243c761ccca6198d1b8d46e19a6
Furqan-Nizamani <furqannizamani001@gmail.com>
2024-04-22 14:05:29 +0500

    commit all commands

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let ToDoList = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "List",
+            type: "input",
+            message: "Please let me knwo what do you want to add in ToDoList."
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want add something extra?",
+            default: "false"
+        }
+    ]);
+    ToDoList.push(addTask.List);
+    condition = addTask.addMore;
+    console.log(ToDoList);
+}
